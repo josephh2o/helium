c64f49a1d473bae815302806ac48550ce6b6de2d
Joseph Ho <79343565+josephh2o@users.noreply.github.com>
2023-02-28 09:49:05 -0600

    Implemented Course Object, Added Additional Output
    Course Object is implemented and functioning as intended. Output now displays a compiled list of assignments from the course list.

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import os
 import pytz
 
 
+# create a new course object
+class Course:
+    def __init__(self, course_code, assignments):
+        self.course_code = course_code
+        self.assignments = assignments
+
+
 def main():
     API_URL = os.getenv("API_URL")
     API_TOKEN = os.getenv("API_TOKEN")
@@ -20,29 +27,34 @@ def main():
     else:
         course_date = "FA" + str(current_date.year)[2:]
 
-    # Find all courses that match the current semester
+    # Create new course object for each course and add to course_list, while sorting assignments by due date
     course_list = []
     for course in courses:
         if not hasattr(course, "enrollment_term_id"):
             continue
         if course_date in course.course_code:
-            course_list.append(course)
+            assignments = course.get_assignments()
+            assignment_list = []
+            for assignment in assignments:
+                if not hasattr(assignment, "due_at") or assignment.due_at is None:
+                    continue
+                if assignment.due_at_date < current_date or assignment.due_at_date > current_date + timedelta(days=7):
+                    continue
+                assignment_list.append(assignment)
+            assignment_list.sort(key=lambda x: x.due_at_date)
+            course_list.append(Course(course.course_code, assignment_list))
 
-    # Find all assignments for each course
+    # Print assignments for the next week in each course
     for course in course_list:
-        assignments = course.get_assignments()
         print("---------------------------------")
         print(course.course_code)
         assignment_list = []
-        for assignment in assignments:
+        for assignment in course.assignments:
             if not hasattr(assignment, "due_at") or assignment.due_at is None:
                 continue
             if assignment.due_at_date < current_date or assignment.due_at_date > current_date + timedelta(days=7):
                 continue
             assignment_list.append(assignment)
-
-        # Sort the assignments by due date
-        assignment_list.sort(key=lambda x: x.due_at_date)
 
         # Print a message if there are no assignments
         if not assignment_list:
@@ -51,6 +63,17 @@ def main():
         # Print all assignments for the course
         for assignment in assignment_list:
             print("\t" + assignment.due_at_date.astimezone().strftime("%m/%d/%Y %H:%M:%S") + "\t" + assignment.name)
+
+    # Sort assignments by due date ignoring the course and print
+    assignment_list = []
+    for course in course_list:
+        for assignment in course.assignments:
+            assignment_list.append(assignment)
+    assignment_list.sort(key=lambda x: x.due_at_date)
+    print("---------------------------------")
+    print("ALL ASSIGNMENTS")
+    for assignment in assignment_list:
+        print("\t" + assignment.due_at_date.astimezone().strftime("%m/%d/%Y %H:%M:%S") + "\t" + assignment.name)
 
 
 if __name__ == "__main__":
